d08276ac38722f6f4ded3c5e443c3f0bb7a5ca3e
Sharath (Munna) Koorathota <sharathckoo@gmail.com>
2020-03-04 19:14:38 -0500

    Update model.py

--- tcav/model.py
+++ tcav/model.py
@@ -121,12 +121,18 @@ class ModelWrapper(six.with_metaclass(ABCMeta, object)):
     The 'ends' and 'bottlenecks_tensors' dictionary should map to tensors
     with the according name.
     """
+    print('---finding bottlenecks---')
     self.bottlenecks_tensors = {}
     self.ends = {}
+    print('node_dict: ', node_dict)
+    print('import_prefix: ', self.import_prefix)
     for k, v in node_dict.iteritems():
+        print('k: ', k, '; v: ', v)
       if self.import_prefix:
         v = 'import/' + v
       tensor = self.sess.graph.get_operation_by_name(v.strip(':0')).outputs[0]
+        print('tensor: ', tensor)
+        
       if k == 'input' or k == 'prediction':
         self.ends[k] = tensor
       else:
