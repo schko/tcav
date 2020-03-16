81d0378eaca96b5ee06efbe18633235facb36513
Sharath (Munna) Koorathota <sharathckoo@gmail.com>
2020-03-15 21:25:30 -0400

    Update tcav.py

--- tcav/tcav.py
+++ tcav/tcav.py
@@ -56,6 +56,7 @@ class TCAV(object):
     # Grad points in the direction which DECREASES probability of class
     print('----getting directional derivative sign inputs----')
     print('mymodel: ', mymodel)
+    print('mymodel.summary: ', mymodel.summary())
     print('act: ', act)
     print('cav: ', cav)
     print('concept: ', concept)
